165a7cf785a1629786ea4593a216a918cbd3cb00
Bruno C <caquinaud.bruno@gmail.com>
2026-07-25 04:25:36 -0700

    New Hello World Test + refactoring

--- Hello-World/hello_test.go
+++ Hello-World/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World"
+	t.Run("Test 1 : Hello Func with string parameter", func(t *testing.T) {
+		got := Hello("Bruno")
+		want := "Hello, Bruno"
 
+		assertCorrectMessage(t, want, got)
+	})
+
+	t.Run("Test 2 : Hello func with empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, Worl"
+
+		assertCorrectMessage(t, want, got)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, want, got string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
